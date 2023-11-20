51b624a8ebb33a45e3e44d0bfde8624b725c1a02
양휘준 <akdlcnd0994@naver.com>
2023-11-20 14:13:37 +0900

    main rebuild

---
+++ s.js
@@ -0,0 +1,26 @@
+const input = document.querySelector(".box_input"),
+    text1 = document.querySelector(".text1"),
+    text2 = document.querySelector(".text2"),
+    text3 = document.querySelector(".text3"),
+    test1 = document.getElementById("test1"),
+    test2 = document.getElementById("test2");
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    var advanced = Math.floor(parseFloat(text1.value)* 0.95 - parseInt(text2.value) * 5);
+    var rare = Math.floor(parseFloat(text1.value) * 0.95 - parseInt(text3.value));
+    var temp = '고급 1셋 구매 시 마진 : ' + advanced + '(5셋 구매 후 교환 기준)';
+    test1.innerHTML = temp;
+    temp = '희귀 1셋 구매 시 마진 : ' + rare
+    test2.innerHTML = temp;
+}
+
+
+function init() {
+    input.addEventListener("submit", handleSubmit);
+}
+
+
+init();
